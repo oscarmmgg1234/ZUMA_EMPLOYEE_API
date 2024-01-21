af78713643d422e7cc9b31f3df6fd4ab01f2d7f8
Oscar Maldonado-Garcia <oscarmmgg1234@gmail.com>
2024-01-20 22:10:33 -0800

    init employee schedule feature

---
+++ src/components/EmployeeSchedule.js
@@ -0,0 +1,4 @@
+// trucate the db entries regarding that users time entries
+// add 4 years worth of time shift entries for every user specified
+// add custom scheduling for every employee regarding start/end shift
+// automate the process of creating entries and pushing while not being destructive and keep robustenes
