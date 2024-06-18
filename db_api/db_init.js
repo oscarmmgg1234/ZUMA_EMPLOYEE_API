7cd0a67522dbe539241a80c1d4c77e0be62785f1
oscar maldonado-garcia <45194139+oscarmmgg1234@users.noreply.github.com>
2024-06-18 11:21:45 -0700

    rollback

--- db_api/db_init.js
+++ db_api/db_init.js
@@ -1,3 +1,4 @@
+
 // const db = mysql.createConnection({
 //   host: "192.168.1.25",
 //   user: "oscy",
@@ -6,68 +7,22 @@
 //   database: "zuma_main",
 // });
 const mysql = require("mysql2");
+// // exports.db = db;
 const fs = require("fs");
+const tls = require("tls");
 
 const sslOptions = {
   ca: fs.readFileSync("./cert/DigiCertGlobalRootCA.crt.pem"), // CA certificate (optional)
   rejectUnauthorized: true, // Reject unauthorized connections (optional, true by default)
   secureProtocol: "TLSv1_2_method", // Specify the TLS version (optional)
 };
-
-// Create a connection pool to your MySQL database
-const pool = mysql.createPool({
+// Create a connection to your MySQL database
+const db = mysql.createConnection({
   host: "zuma.mysql.database.azure.com",
   user: "oscar",
   port: 3306,
   password: "Omariscool1234!",
   database: "zuma_main",
   ssl: sslOptions,
-  waitForConnections: true,
-  connectionLimit: 10,
-  queueLimit: 0,
 });
-
-const promisePool = pool.promise();
-
-// Function to keep the connection alive
-const keepAliveQuery = async () => {
-  try {
-    await promisePool.query("SELECT 1");
-  } catch (err) {
-    console.log("Error with keep-alive query:", err);
-  }
-};
-
-// Keep the connection alive by sending a query every 60 seconds
-setInterval(keepAliveQuery, 60000); // every 60 seconds
-
-// Wrapper object to mimic the original db interface
-const db = {
-  connect: (callback) => {
-    // Simulate the connect method for compatibility
-    if (callback) callback();
-  },
-  query: (sql, params, callback) => {
-    return promisePool
-      .query(sql, params)
-      .then(([rows, fields]) => {
-        if (callback) callback(null, rows, fields);
-      })
-      .catch((err) => {
-        if (callback) callback(err);
-      });
-  },
-  execute: (sql, params, callback) => {
-    return promisePool
-      .execute(sql, params)
-      .then(([result]) => {
-        if (callback) callback(null, result);
-      })
-      .catch((err) => {
-        if (callback) callback(err);
-      });
-  },
-  end: () => promisePool.end(),
-};
-
-exports.db_init = () => db;
+exports.db_init = () => {return db};
