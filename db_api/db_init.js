89fdcb7176ba3a9c88982638162a21867a5044d3
oscar maldonado-garcia <45194139+oscarmmgg1234@users.noreply.github.com>
2024-06-18 11:25:38 -0700

    rollback

--- db_api/db_init.js
+++ db_api/db_init.js
@@ -7,43 +7,22 @@
 //   database: "zuma_main",
 // });
 const mysql = require("mysql2");
+// // exports.db = db;
 const fs = require("fs");
+const tls = require("tls");
 
 const sslOptions = {
   ca: fs.readFileSync("./cert/DigiCertGlobalRootCA.crt.pem"), // CA certificate (optional)
   rejectUnauthorized: true, // Reject unauthorized connections (optional, true by default)
   secureProtocol: "TLSv1_2_method", // Specify the TLS version (optional)
 };
-
-let connection;
-
-function handleDisconnect() {
-  connection = mysql.createConnection({
-    host: "zuma.mysql.database.azure.com",
-    user: "oscar",
-    port: 3306,
-    password: "Omariscool1234!",
-    database: "zuma_main",
-    ssl: sslOptions,
-  });
-
-  connection.connect((err) => {
-    if (err) {
-      console.log("Error connecting to MySQL:", err);
-      setTimeout(handleDisconnect, 2000);
-    }
-  });
-
-  connection.on("error", (err) => {
-    console.log("MySQL error:", err);
-    if (err.code === "PROTOCOL_CONNECTION_LOST") {
-      handleDisconnect();
-    } else {
-      throw err;
-    }
-  });
-}
-
-handleDisconnect();
-
-exports.db_init = () => connection;
+// Create a connection to your MySQL database
+const db = mysql.createConnection({
+  host: "zuma.mysql.database.azure.com",
+  user: "oscar",
+  port: 3306,
+  password: "Omariscool1234!",
+  database: "zuma_main",
+  ssl: sslOptions,
+});
+exports.db_init = () => {return db};
