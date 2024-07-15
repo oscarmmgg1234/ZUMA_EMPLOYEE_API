46df27b5f4fc43c8c67c0577a6002fb16cb7c496
oscar maldonado-garcia <45194139+oscarmmgg1234@users.noreply.github.com>
2024-07-15 07:43:34 -0700

    changed db source

--- db_api/db_init.js
+++ db_api/db_init.js
@@ -1,4 +1,3 @@
-
 // const db = mysql.createConnection({
 //   host: "192.168.1.25",
 //   user: "oscy",
@@ -8,21 +7,23 @@
 // });
 const mysql = require("mysql2");
 // // exports.db = db;
-const fs = require("fs");
-const tls = require("tls");
+// const fs = require("fs");
+// const tls = require("tls");
 
-const sslOptions = {
-  ca: fs.readFileSync("./cert/DigiCertGlobalRootCA.crt.pem"), // CA certificate (optional)
-  rejectUnauthorized: true, // Reject unauthorized connections (optional, true by default)
-  secureProtocol: "TLSv1_2_method", // Specify the TLS version (optional)
-};
+// const sslOptions = {
+//   ca: fs.readFileSync("./cert/DigiCertGlobalRootCA.crt.pem"), // CA certificate (optional)
+//   rejectUnauthorized: true, // Reject unauthorized connections (optional, true by default)
+//   secureProtocol: "TLSv1_2_method", // Specify the TLS version (optional)
+// };
 // Create a connection to your MySQL database
 const db = mysql.createConnection({
-  host: "zuma.mysql.database.azure.com",
+  host: "192.168.1.218",
   user: "oscar",
   port: 3306,
   password: "Omariscool1234!",
   database: "zuma_main",
-  ssl: sslOptions,
+  // ssl: sslOptions,
 });
-exports.db_init = () => {return db};
+exports.db_init = () => {
+  return db;
+};
