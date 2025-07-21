626b2a18ab884c6bc2efdcee7ba78b676d517e01
Oscar Maldonado <oscarmmgg1234@gmail.com>
2025-07-21 12:45:47 -0700

    updates

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -5,8 +5,6 @@ module.exports = {
       script: "index.js",
       exec_mode: "fork", // Restart at 2:00 AM every day
       // Other configurations like instances, exec_mode etc.
-      wait_ready: true,
-      post_delay: 15000,
     },
   ],
 };
